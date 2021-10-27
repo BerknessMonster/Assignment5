5ca3778b6812a72af8efbb77d40cb6e254dce24c
DESKTOP-M11KKJ7\JonBe <jberkness16@winona.edu>
2021-10-27 12:49:46 -0500

    rejhwkejrhgkwjsehrgkwjsehr

---
+++ Assignment4444/src/com/jonny/NormalUser.java
@@ -0,0 +1,13 @@
+package com.jonny;
+
+public class NormalUser extends User {
+
+	public NormalUser (String username, String password, String name) {
+		this.role = "normal_user";
+		this.setUsername(username);
+		this.setPassword(password);
+		this.setName(name);
+	
+
+	}
+}
