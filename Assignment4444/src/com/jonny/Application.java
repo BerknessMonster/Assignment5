6d3991aae8f13b5f3a78b0ad2cdd002b7177df9e
DESKTOP-M11KKJ7\JonBe <jberkness16@winona.edu>
2021-11-01 13:52:47 -0500

    added finally, and messed with user service.

--- Assignment4444/src/com/jonny/Application.java
+++ Assignment4444/src/com/jonny/Application.java
@@ -20,8 +20,12 @@ public class Application {
 			 user = new User();
 				while ((lineBeingRead = bufferedReader.readLine()) != null) {
 					String[] usersSplit = new String[4];
+				//	String[] usersSplit = line.split(" , ");
+				//	if (usersSplit.equals(lineBeingRead[4]));
 					String userInfo;
 					userInfo.split(lineBeingRead);
+				//	userInfo.split(", ");
+				//	lineBeingRead.split(userInfo);
 					 user.setName(lineBeingRead);
 					System.out.println(user.getName());
 				}	
@@ -30,6 +34,47 @@ public class Application {
 			System.out.println("file not read");
 			e.printStackTrace();
 		}
+			finally {
+				if (bufferedReader != null)
+					bufferedReader.close();
+			}
 	}
-
+//	 String promptUsernameToUpdate() {
+//		System.out.println("Which user would you like to login as? (Type in a valid username)");
+//		String usernameToUpdate = scanner.nextLine();
+//		return usernameToUpdate;
+//	}
+//
+//	 promptUpdateName(User loggedInUser) {
+//		System.out.println("Please type in your new name: ");
+//		String name = scanner.nextLine();
+//		loggedInUser.setName(name);
+//	}
+//
+//	promptUpdatePassword(User loggedInUser) {
+//		System.out.println("Please type in your new password: ");
+//		String password = scanner.nextLine();
+//		loggedInUser.setPassword(password);
+//	}
+//
+//	 promptUpdateUsername(User loggedInUser) {
+//		System.out.println("Please type in your new username: ");
+//		String username = scanner.nextLine();
+//		loggedInUser.setUsername(username);
+//	}
+//
+//	 int promptOptions(User loggedInUser) {
+//		System.out.println("----------");
+//		System.out.println("Please choose from the following options:");
+//		if (loggedInUser instanceof SuperUser) {
+//			System.out.println("(0) Log in as another user ");
+//		}
+//		System.out.println("(1) Update username");
+//		System.out.println("(2) Update password");
+//		System.out.println("(3) Update name");
+//		System.out.println("(4) Exit");
+//		String option = scanner.nextLine();
+//		
+//		return Integer.parseInt(option);
+//	}
 }
