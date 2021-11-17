a24697ac0339f89e3692601e6eddfcd193716226
DESKTOP-M11KKJ7\JonBe <jberkness16@winona.edu>
2021-11-17 13:51:22 -0600

    I'm stuck

--- Assignment4444/src/com/jonny/UserService.java
+++ Assignment4444/src/com/jonny/UserService.java
@@ -2,7 +2,7 @@ package com.jonny;
 
 import java.util.Scanner;
 
-public class UserService {
+public class UserService<user> {
 	private Scanner scanner = new Scanner(System.in);
 
 
@@ -24,7 +24,6 @@ public class UserService {
 				if (userInputEmail.equalsIgnoreCase(user.getUsername()) && userInputPassword.equals(user.getPassword()))
 					System.out.println("Welcome " + user.getName());
 				return user;
-				
 			}
 			if (loggedInUser == false) {
 				System.out.println("Please enter a valid username and password");
@@ -34,54 +33,40 @@ public class UserService {
 		return null;
 
 	}
-//   public void loggedInUserPrompts (User users, Scanner scanner, User loggedInUser) {
-//    int option = 0;	
-//	While (loggedinUser == true && super user == true && option != 4) {
-//		give super prompts..
-//		private void loginAsAnotherUser () {
-//		System.out.println("Who would you like to login as?");
-//		
-//	}
-//		private void updateUsername () {
-//		System.out.println("What would you like your new username to be?");
-//	}
-//		private void updatePassword () {
-//		System.out.println("What would you like your new password to be?");
-//	}
-//		private void updateName () {
-//		System.out.println("What would you like your new name to be?");
-//	}
-//		optionExit () {
-//		System.exit
-//	}
-//      
-//		While (loggedInUser == true && super user = false) {
-//		private void updateUsername () {
-//	System.out.println("Please type in your new username: ");
-//	String username = scanner.nextLine();
-//	loggedInUser.setUsername(username);
-//	}
 	
-//		private void updatePassword () {
-//	}
-//		private void updateName () {
-//	}
-//		optionExit () {
-//	}
-//}     
-//}
-//	//private int loggedInUserPromptOptions(User loggedInUser) {
-//	System.out.println("----------");
-//	System.out.println("Please choose from the following options:");
-//	if (loggedInUser instanceof SuperUser) {
-//	  System.out.println("(0) Log in as another user ");
-//	}
-//	System.out.println("(1) Update username");
-//	System.out.println("(2) Update password");
-//	System.out.println("(3) Update name");
-//	System.out.println("(4) Exit");
-//	String option = scanner.nextLine();
-//	
-//	return Integer.parseInt(option);
-//}
-}
+	
+		private void updateUsername (user loggedInUser) {
+		System.out.println("What would you like your new username to be?");
+		String username = scanner.nextLine();
+		((User) loggedInUser).setUsername(username);
+	}
+		
+		private void updatePassword (user loggedInUser) {
+		System.out.println("What would you like your new password to be?");
+		String password = scanner.nextLine();
+		((User) loggedInUser).setPassword(password);
+	}
+		private void updateName (user loggedInUser) {
+		System.out.println("What would you like your new name to be?");
+		String name = scanner.nextLine();
+		((User) loggedInUser).setName(name);
+	}
+		private int loggedInUserPromptOptions(User loggedInUser) {
+			System.out.println("----------");
+			System.out.println("Please choose from the following options:");
+			if (loggedInUser instanceof SuperUser) {
+				System.out.println("(0) Log in as another user ");
+			}
+			System.out.println("(1) Update username");
+			System.out.println("(2) Update password");
+			System.out.println("(3) Update name");
+			System.out.println("(4) Exit");
+			String option = scanner.nextLine();
+			return Integer.parseInt(option);
+			
+		}
+	}
+      
+		
+
+
